771617d294d7b88f237a3767332787c99b98f952
rajur <rajur@LAPTOP-JTDREUID>
2025-11-11 20:38:23 +0530

    Meaningful message about your update

--- App.js
+++ App.js
@@ -1,21 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement(
   "div",
-  { id: "parent" },[
-  React.createElement(
-    "div",
-    { id: "child1" },
-    [
+  { id: "parent" },
+  [
+    React.createElement("div", { id: "child1" }, [
       React.createElement("h1", {}, "I am h1 tag"),
-      React.createElement("h2", {}, "I am h2 tag")
-    ]
-  ),React.createElement(
-    "div",
-    { id: "child2" },
-    [
+      React.createElement("h2", {}, "I am h2 tag"),
+    ]),
+    React.createElement("div", { id: "child2" }, [
       React.createElement("h1", {}, "I am h1 tag"),
-      React.createElement("h2", {}, "I am h2 tag")
-    ]
-  )]
+      React.createElement("h2", {}, "I am h2 tag"),
+    ]),
+  ]
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
---
+++ README.md
@@ -0,0 +1,30 @@
+# parcel
+ -Dev Build
+ -Local Server
+ -Hot Module Replacement
+ -File watching Algorithm -written in c++
+ -caching Faster Builds
+ -Image Optimization 
+ -Minification
+ -Bundling
+ -Compress
+ -Consistent Hashing
+ -Code
+ -Codeliting
+ -Differential bundling -support older browsers
+ -Diagnostic
+ -Error handing
+ -Https
+ -Reliable Chache
+ -Tree Sheking -remove unused code
+ -Different Dev and Prod Bundles
+ 
+
+
+
+
+
+#  --npm init
+# --npm install react
+
+ # --npx parcel index.html
